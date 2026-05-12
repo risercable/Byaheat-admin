f652f1ea786c3397191b1f76ed67426979cf75ac
jmbul <jerald.mark.buljatin@gmail.com>
2026-05-12 17:35:38 +0800

    Removed console log

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -3,8 +3,6 @@ require('dotenv').config();
 const admin = require('firebase-admin');
 const serviceAccount = require('./xxxxxxxxxxxxxxxxxxxxx.json');
 
-console.log("DB URL:", process.env.FIREBASE_DATABASE_URL);
-
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
